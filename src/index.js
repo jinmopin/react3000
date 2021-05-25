d98fa81a1ec7152a60530f019c76b9d26e6d7af8
jinmopin <80299291+jinmopin@users.noreply.github.com>
2021-05-25 13:18:22 +0900

    2021.05.25

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-function Welcome(props) {
-    return <h1>Hello, {props.name}</h1>
-}
-
-const element = <Welcome name="jinjaehyun" />;
-
-ReactDOM.render(element, document.getElementById("root"));
+import React from 'react';
+    import ReactDOM from 'react-dom';
+    function formatDate(date){
+        return date.toLocaleDateString();
+    }
+    function Comment(props) {
+        return (
+            <div className="Comment">
+                <div className="user-info">
+                    <img 
+                        className="avatar" 
+                        src={props.author.avatarUrl} 
+                        alt={props.author.name} 
+                    />
+                    <div className="user-name">{props.author.name}</div>
+                </div>
+                <div className="comment-text">{props.text}</div>
+                <div className="comment-date">{formatDate(props.date)}</div>
+            </div>
+        );
+    }
+    
+    const comment = {
+        date: new Date(),
+        text: "열심히 할수록 기회는 따른다.",
+        author: {
+            name: "jinjeahyun",
+            avatarUrl: "http://jinmopin.dothome.co.kr/portfolio/assets/images/aboutImg01.jpg"
+        },
+    };
+    
+    
+    ReactDOM.render(
+    <Comment date={comment.date} text={comment.text} author={comment.author} />, document.getElementById("root"));    
