729331772ec77edf85332a0f144284675771359c
jinmopin <80299291+jinmopin@users.noreply.github.com>
2021-05-25 12:33:21 +0900

    2021.05.25

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>
+}
+
+const element = <Welcome name="jinjaehyun" />;
+
+ReactDOM.render(element, document.getElementById("root"));
