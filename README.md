690a1a9a6595ab875dd9f35917e62fcd1674993e
jinmopin <80299291+jinmopin@users.noreply.github.com>
2021-05-26 11:01:56 +0900

    2021.05.26

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # 리액트 시작하기
+
+## 로우터 설정
+npm install react-router-dom
+## 프롭타입 설정 (변수타입 설정)
+npm install prop-types
+## Sass 설정
+npm install node-sass
--- src/index.js
+++ src/index.js
@@ -1,45 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function formatDate(date){
-    return date.toLocaleDateString();
+class Clock extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>Hello</h1>
+                <h2>지금은 {this.props.date.toLocaleTimeString()} 입니다.</h2>
+            </div>
+        )
+    }
 }
 
-function Avatar(props){
-    return (
-        <img src={props.user.avatarUrl} alt={props.user.name} />
-    )
+function tick() {
+    ReactDOM.render(<Clock date={new Date()} />, document.getElementById("root"));
 }
 
-function UserInfo(props){
-    return (
-        <div className="UserInfo">
-            <Avatar user={props.user} />
-            <div>{props.user.name}</div>
-        </div>
-    )
-}
-
-function Comment(props){
-    return (
-        <div className="Comment">
-            <UserInfo user={props.author}/>
-            <div className="Comment-text">{props.text}</div>
-            <div className="Comment-date">{formatDate(props.date)}</div>
-        </div>
-    )
-
-}
-
-const comment = {
-    date: new Date(),
-    text: "열심히 할수록 기회는 따른다.",
-    author: {
-        name: "jinjeahyun",
-        avatarUrl: "https://images.unsplash.com/photo-1621536816171-72c8ea3d63cf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=664&q=80"
-    },
-};
-    
-
-ReactDOM.render(<Comment date={comment.date} text={comment.text} author={comment.author}/>, document.getElementById("root"));
+setInterval(tick, 1000);
 
