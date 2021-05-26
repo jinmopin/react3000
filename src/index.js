1633480698ee59b095d2a2fe6d23bc9018785ec0
jinmopin <80299291+jinmopin@users.noreply.github.com>
2021-05-26 10:14:18 +0900

    2021.05.26

--- src/index.js
+++ src/index.js
@@ -1,34 +1,45 @@
 import React from 'react';
-    import ReactDOM from 'react-dom';
-    function formatDate(date){
-        return date.toLocaleDateString();
-    }
-    function Comment(props) {
-        return (
-            <div className="Comment">
-                <div className="user-info">
-                    <img 
-                        className="avatar" 
-                        src={props.author.avatarUrl} 
-                        alt={props.author.name} 
-                    />
-                    <div className="user-name">{props.author.name}</div>
-                </div>
-                <div className="comment-text">{props.text}</div>
-                <div className="comment-date">{formatDate(props.date)}</div>
-            </div>
-        );
-    }
+import ReactDOM from 'react-dom';
+
+function formatDate(date){
+    return date.toLocaleDateString();
+}
+
+function Avatar(props){
+    return (
+        <img src={props.user.avatarUrl} alt={props.user.name} />
+    )
+}
+
+function UserInfo(props){
+    return (
+        <div className="UserInfo">
+            <Avatar user={props.user} />
+            <div>{props.user.name}</div>
+        </div>
+    )
+}
+
+function Comment(props){
+    return (
+        <div className="Comment">
+            <UserInfo user={props.author}/>
+            <div className="Comment-text">{props.text}</div>
+            <div className="Comment-date">{formatDate(props.date)}</div>
+        </div>
+    )
+
+}
+
+const comment = {
+    date: new Date(),
+    text: "열심히 할수록 기회는 따른다.",
+    author: {
+        name: "jinjeahyun",
+        avatarUrl: "https://images.unsplash.com/photo-1621536816171-72c8ea3d63cf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=664&q=80"
+    },
+};
     
-    const comment = {
-        date: new Date(),
-        text: "열심히 할수록 기회는 따른다.",
-        author: {
-            name: "jinjeahyun",
-            avatarUrl: "http://jinmopin.dothome.co.kr/portfolio/assets/images/aboutImg01.jpg"
-        },
-    };
-    
-    
-    ReactDOM.render(
-    <Comment date={comment.date} text={comment.text} author={comment.author} />, document.getElementById("root"));    
+
+ReactDOM.render(<Comment date={comment.date} text={comment.text} author={comment.author}/>, document.getElementById("root"));
+
