3ccd932232d96c390dbd1f45b1f97b79e5f43e1d
jinmopin <80299291+jinmopin@users.noreply.github.com>
2021-05-27 10:04:02 +0900

    2021.05.27-3

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
 # 리액트 시작하기
 
 ## 리액트 앱 만들기
+
 npx create-react-app 앱 이름
 
 ## 로우터 설정
+
 npm install react-router-dom
 
 ## 프롭타입 설정 (변수타입 설정)
+
 npm install prop-types
 
 ## Sass 설정
+
 npm install node-sass
+
+## ESLINT, prettier
+
+설치 후 save 관련 체크, saveformat 설정, Beautify 삭제
--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,36 +1,33 @@
-import { checkPropTypes, func } from 'prop-types';
-import React from 'react';
-import Header from '../Header';
-import Layout from '../Layout';
+import React from "react";
+import Header from "../Header";
+import Layout from "../Layout";
 
-
-function Info({text}) {
-    return <div>{text}</div>;
+function Info({ text }) {
+  return <div>{text}</div>;
 }
-        
+
 const textInfo = [
-    {text : "WE PROVIDE"}, 
-    {text : "VISUAL CODING"},
-    {text : "SOLUTION"},
-    {text : "FOR YOU WEBS"},
+  { text: "WE PROVIDE" },
+  { text: "VISUAL CODING" },
+  { text: "SOLUTION" },
+  { text: "FOR YOU WEBS" }, 
 ];
 
-function Main(){
-    return (
-        <div id="wrap">
-            <Header />
-            <Layout>
-            <section id="mainCont">
-                <div className="main__cont">
-                    {textInfo.map((el) => (
-                        <Info text={el.text} />
-                    ))}
-                    
-                </div>
-            </section>
-            </Layout>
-        </div>
-    )
+function Main() {
+  return (
+    <div id="wrap">
+      <Header />
+      <Layout>
+        <section id="mainCont">
+          <div className="main__cont">
+            {textInfo.map((el) => (
+                <Info text={el.text} />
+            ))}
+          </div>
+        </section>
+      </Layout>
+    </div>
+  );
 }
 
 export default Main;
