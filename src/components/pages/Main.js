050abbd335552640df4c1133c40c2fbdcee8eb6e
jinmopin <80299291+jinmopin@users.noreply.github.com>
2021-05-27 09:53:44 +0900

    2021.05.27-2

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -3,33 +3,17 @@ import React from 'react';
 import Header from '../Header';
 import Layout from '../Layout';
 
-// function Info({ text1, text2, text3, text4 }) {
-//     return (
-//         <div className="main__cont">
-//             <div>{text1}</div>
-//             <div>{text2}</div>
-//             <div>{text3}</div>
-//             <div>{text4}</div>
-//         </div>
-//     );
-// }
-function Info(props) {
-    return (
-        <div className="main__cont">
-            <div>{props.text1}</div>
-            <div>{props.text2}</div>
-            <div>{props.text3}</div>
-            <div>{props.text4}</div>
-        </div>
-    );
-}
 
-const textInfo = {
-    text1 : "WE PROVIDE", 
-    text2 : "VISUAL CODING",
-    text3 : "SOLUTION" ,
-    text4 : "FOR YOU WEBS",
-};
+function Info({text}) {
+    return <div>{text}</div>;
+}
+        
+const textInfo = [
+    {text : "WE PROVIDE"}, 
+    {text : "VISUAL CODING"},
+    {text : "SOLUTION"},
+    {text : "FOR YOU WEBS"},
+];
 
 function Main(){
     return (
@@ -38,12 +22,10 @@ function Main(){
             <Layout>
             <section id="mainCont">
                 <div className="main__cont">
-                    <Info 
-                        text1={textInfo.text1}
-                        text2={textInfo.text2}
-                        text3={textInfo.text3}
-                        text4={textInfo.text4}
-                    />
+                    {textInfo.map((el) => (
+                        <Info text={el.text} />
+                    ))}
+                    
                 </div>
             </section>
             </Layout>
